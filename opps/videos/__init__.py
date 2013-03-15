cbd279ab9fa25219df562b1d99e910c65bd759ec
Sergio Oliveira <sergio@tracy.com.br>
2013-03-15 18:03:36 -0300

    First commit of videos app

---
+++ opps/videos/__init__.py
@@ -0,0 +1,15 @@
+
+import pkg_resources
+
+pkg_resources.declare_namespace(__name__)
+
+VERSION = (0, 1, 1)
+
+__version__ = ".".join(map(str, VERSION))
+__status__ = "Development"
+__description__ = u"Videos App for Opps CMS"
+
+__author__ = u"Sergio Oliveira"
+__credits__ = []
+__email__ = u"sergio@tracy.com.br"
+__copyright__ = u"Copyright 2013, YACOWS"
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+from opps import videos
+
+
+
+install_requires = ["opps"]
+
+classifiers = ["Development Status :: 4 - Beta",
+               "Intended Audience :: Developers",
+               "Operating System :: OS Independent",
+               "Framework :: Django",
+               'Programming Language :: Python',
+               "Programming Language :: Python :: 2.7",
+               "Operating System :: OS Independent",
+               "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+               'Topic :: Software Development :: Libraries :: Python Modules']
+
+try:
+    long_description = open('README.md').read()
+except:
+    long_description = videos.__description__
+
+setup(name='opps-videos',
+        namespace_packages=['opps'],
+        version=videos.__version__,
+        description=videos.__description__,
+        long_description=long_description,
+        classifiers=classifiers,
+        keywords='Video upload app for opps cms',
+        author=videos.__author__,
+        author_email=videos.__email__,
+        packages=find_packages(exclude=('doc', 'docs',)),
+        install_requires=install_requires,
+        include_package_data=True,)
