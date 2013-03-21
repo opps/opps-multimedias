7dfe0fd5a6931833768060f7fdccd965f154590f
Sergio Oliveira <sergio@tracy.com.br>
2013-03-21 15:15:15 -0300

    Adding Video model

--- opps/videos/models.py
+++ opps/videos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from opps.article.models import Article
+
+class Video(Article):
+    length = models.PositiveIntegerField(_(u'Length'))
+    name = models.CharField(_(u'Name'), max_length='255')
