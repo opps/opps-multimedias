98b04ad5e9da193f079b05114eaf499dc4197af6
Sergio Oliveira <sergio@tracy.com.br>
2013-03-25 10:35:54 -0300

    Video uploads to youtube

--- opps/videos/admin.py
+++ opps/videos/admin.py
@@ -4,6 +4,7 @@ import ffvideo
 from django import forms
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.fields.files import FieldFile
 from djcelery.models import (TaskState, WorkerState,
                  PeriodicTask, IntervalSchedule, CrontabSchedule)
 from opps.article.admin import ArticleAdmin
@@ -15,23 +16,36 @@ class VideoAdminForm(forms.ModelForm):
     class Meta:
         model = Video
 
+    def _get_video_path(self):
+        video_file = self.cleaned_data.get('video_file')
+        if not video_file:
+            return
+
+        if isinstance(video_file, FieldFile):
+            return video_file.path
+
+        return video_file.temporary_file_path()
+
+
     def clean_video_file(self):
         video_file = self.cleaned_data['video_file']
         try:
-            vs = ffvideo.VideoStream(video_file.temporary_file_path())
+            vs = ffvideo.VideoStream(self._get_video_path())
         except ffvideo.DecoderError:
             raise forms.ValidationError(_('Invalid video format'))
         return video_file
 
     def clean(self):
-        video_file = self.cleaned_data.get('video_file')
-        if video_file:
-            vs = ffvideo.VideoStream(video_file.temporary_file_path())
+        video_path = self._get_video_path()
+        if video_path:
+            vs = ffvideo.VideoStream(video_path)
             self.cleaned_data['length'] = int(vs.duration)
         return self.cleaned_data
 
+
 class VideoAdmin(ArticleAdmin):
     form = VideoAdminForm
+    change_form_template = 'videos/admin/change_form.html'
 
     fieldsets = (
         (_(u'Identification'), {
@@ -47,6 +61,15 @@ class VideoAdmin(ArticleAdmin):
             'fields': ('published', 'date_available')}),
     )
 
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        video = Video.objects.get(pk=object_id)
+
+        extra_context = extra_context or {}
+        extra_context['upload_status'] = video.celery_result.status
+
+        return super(VideoAdmin, self).change_view(request, object_id,
+            form_url, extra_context=extra_context)
+
 admin.site.register(Video, VideoAdmin)
 
 
@@ -56,3 +79,4 @@ admin.site.unregister(WorkerState)
 admin.site.unregister(IntervalSchedule)
 admin.site.unregister(CrontabSchedule)
 admin.site.unregister(PeriodicTask)
+
--- opps/videos/models.py
+++ opps/videos/models.py
@@ -1,8 +1,15 @@
 
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 
+from celery.result import AsyncResult
+
 from opps.article.models import Article
+from .tasks import upload_video
+from .youtube import Youtube
+
 
 class Video(Article):
 
@@ -24,3 +31,37 @@ class Video(Article):
     host = models.CharField(_('Host'), max_length=16, choices=HOST_CHOICES,
                             default=HOST_UOLMAIS,
                             help_text=_('Provider that will store the video'))
+    host_id = models.CharField(_('Host ID'), max_length=64, null=True)
+    host_url = models.URLField(max_length=255, null=True)
+    host_swf_url = models.URLField(max_length=255, null=True)
+
+    celery_task_id = models.CharField(_('Upload Task ID'), max_length=64,
+                                      null=True)
+
+    @property
+    def hostctl(self):
+        if self.host == Video.HOST_YOUTUBE:
+            return Youtube(self)
+
+    @property
+    def celery_result(self):
+        return AsyncResult(self.celery_task_id)
+
+    def update_host(self):
+        # Without a host_id we can't update anything at the host
+        if not self.host_id:
+            return
+
+        if self.published:
+            self.hostctl.publish()
+        else:
+            self.hostctl.unpublish()
+
+    def save(self, *args, **kwargs):
+        super(Video, self).save(*args, **kwargs)
+        if not self.celery_task_id:
+            async_res = upload_video.delay(self)
+            self.celery_task_id = async_res.id
+            self.save()
+        else:
+            self.update_host()
