5136baf25d22f357a98b3aacc1e69c4353aa4733
Bruno Rocha <rochacbruno@gmail.com>
2013-05-06 18:10:45 -0300

    fix conflict in urls

--- opps/multimedias/urls.py
+++ opps/multimedias/urls.py
@@ -7,13 +7,13 @@ from .views import VideoDetail, AudioDetail, VideoList, AudioList
 
 urlpatterns = patterns(
     '',
-    url(r'audio/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
+    url(r'^audio/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
         cache_page(60 * 15)(AudioDetail.as_view()), name='audio_detail'),
-    url(r'video/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
+    url(r'^video/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
         cache_page(60 * 15)(VideoDetail.as_view()), name='video_detail'),
 
-    url(r'video/(?P<channel__long_slug>[\w\b//-]+)/$',
+    url(r'^video/(?P<channel__long_slug>[\w\b//-]+)/$',
         VideoList.as_view(), name='video_list'),
-    url(r'audio/(?P<channel__long_slug>[\w\b//-]+)/$',
+    url(r'^audio/(?P<channel__long_slug>[\w\b//-]+)/$',
         AudioList.as_view(), name='audio_list'),
 )
