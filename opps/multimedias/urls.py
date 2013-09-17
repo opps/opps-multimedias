1d3a708c5645bfa5c369721dda612d297cb20b4e
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-17 19:03:44 -0300

    set cache in url

--- opps/multimedias/urls.py
+++ opps/multimedias/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 from django.views.decorators.cache import cache_page
+from django.conf import settings
 
 from .views import (VideoDetail, AudioDetail, VideoList, AudioList,
                     AllVideoList, AllAudioList)
@@ -9,9 +10,11 @@ from .views import (VideoDetail, AudioDetail, VideoList, AudioList,
 urlpatterns = patterns(
     '',
     url(r'^audio/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
-        cache_page(60 * 15)(AudioDetail.as_view()), name='audio_detail'),
+        cache_page(settings.OPPS_CACHE_EXPIRE)(
+            AudioDetail.as_view()), name='audio_detail'),
     url(r'^video/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
-        cache_page(60 * 15)(VideoDetail.as_view()), name='video_detail'),
+        cache_page(settings.OPPS_CACHE_EXPIRE)(
+            VideoDetail.as_view()), name='video_detail'),
 
     url(r'^video/(?P<channel__long_slug>[\w\b//-]+)/$',
         VideoList.as_view(), name='video_list'),
@@ -19,7 +22,9 @@ urlpatterns = patterns(
         AudioList.as_view(), name='audio_list'),
 
     url(r'^videos/$',
-        AllVideoList.as_view(), name='videos_list'),
+        cache_page(settings.OPPS_CACHE_EXPIRE)(
+            AllVideoList.as_view()), name='videos_list'),
     url(r'^audios/$',
-        AllAudioList.as_view(), name='audios_list'),
+        cache_page(settings.OPPS_CACHE_EXPIRE)(
+            AllAudioList.as_view()), name='audios_list'),
 )
