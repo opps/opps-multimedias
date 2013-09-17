fd976edaf894d2e4ec88405c140cc4e3d147c6c7
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-17 19:22:00 -0300

    add feed url enter to video/audio

--- opps/multimedias/urls.py
+++ opps/multimedias/urls.py
@@ -3,12 +3,21 @@ from django.conf.urls import patterns, url
 from django.views.decorators.cache import cache_page
 from django.conf import settings
 
+from opps.contrib.feeds.views import ContainerFeed, ChannelFeed
+
 from .views import (VideoDetail, AudioDetail, VideoList, AudioList,
                     AllVideoList, AllAudioList)
 
 
 urlpatterns = patterns(
     '',
+    url(r'^video/(?P<long_slug>[\w\b//-]+)/(rss|feed)$',
+        cache_page(settings.OPPS_CACHE_EXPIRE)(
+            ChannelFeed()), name='video_list_feed'),
+    url(r'^audio/(?P<long_slug>[\w\b//-]+)/(rss|feed)$',
+       cache_page(settings.OPPS_CACHE_EXPIRE)(
+           ChannelFeed()), name='audio_list_feed'),
+
     url(r'^audio/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
         cache_page(settings.OPPS_CACHE_EXPIRE)(
             AudioDetail.as_view()), name='audio_detail'),
@@ -20,6 +29,13 @@ urlpatterns = patterns(
         VideoList.as_view(), name='video_list'),
     url(r'^audio/(?P<channel__long_slug>[\w\b//-]+)/$',
         AudioList.as_view(), name='audio_list'),
+
+    url(r'^videos/(rss|feed)$',
+        cache_page(settings.OPPS_CACHE_EXPIRE)(
+            ContainerFeed('Video')), name='videos_list_feed'),
+    url(r'^audios/(rss|feed)$',
+        cache_page(settings.OPPS_CACHE_EXPIRE)(
+            ContainerFeed('Audio')), name='audios_list_feed'),
 
     url(r'^videos/$',
         cache_page(settings.OPPS_CACHE_EXPIRE)(
