1bf2a82b325c32068603165d3390c060b4fecc25
Paulo R <proberto.macedo@gmail.com>
2014-08-03 23:01:49 +0200

    Fixes search_indexes

--- opps/multimedias/search_indexes.py
+++ opps/multimedias/search_indexes.py
@@ -1,40 +1,17 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.utils import timezone
-from haystack.indexes import SearchIndex, Indexable, CharField, DateTimeField
+
+from haystack.indexes import Indexable
+
+from opps.containers.search_indexes import ContainerIndex
 
 from .models import Audio, Video
 
 
-class AudioIndex(SearchIndex, Indexable):
-    text = CharField(document=True, use_template=True)
-    date_available = DateTimeField(model_attr='date_available')
-    date_update = DateTimeField(model_attr='date_update')
-
-    def get_updated_field(self):
-        return 'date_update'
-
+class AudioIndex(ContainerIndex, Indexable):
     def get_model(self):
         return Audio
 
-    def index_queryset(self, using=None):
-        return self.get_model().objects.filter(
-            date_available__lte=timezone.now(),
-            published=True)
 
-
-class VideoIndex(SearchIndex, Indexable):
-    text = CharField(document=True, use_template=True)
-    date_available = DateTimeField(model_attr='date_available')
-    date_update = DateTimeField(model_attr='date_update')
-
-    def get_updated_field(self):
-        return 'date_update'
-
+class VideoIndex(ContainerIndex, Indexable):
     def get_model(self):
         return Video
-
-    def index_queryset(self, using=None):
-        return Video.objects.filter(
-            date_available__lte=timezone.now(),
-            published=True)
