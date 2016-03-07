52a31ffa7eb3e464761066508e525c2786ba939b
William <williamalvesroot@gmail.com>
2016-03-07 20:10:22 +0000

    Add a optional timeout settings on vimeo upload

---
+++ opps/multimedias/timeout.py
@@ -0,0 +1,25 @@
+import signal
+
+
+class TimeoutError(Exception):
+    pass
+
+
+class Timeout(object):
+    """
+    Timeout limit to exec a function
+    NOTE: signals workn only on unix based plataforms
+    """
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
